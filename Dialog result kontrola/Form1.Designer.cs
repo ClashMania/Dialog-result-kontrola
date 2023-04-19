e931fd53e510b3f491b90aa11c00e994d9ae32aa
ClashMania <113007885+ClashMania@users.noreply.github.com>
2023-04-19 08:35:13 +0200

    Add files via upload

---
+++ Dialog result kontrola/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace DialogResultKontrola
+{
+    partial class FormDialogResult
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOtvori = new System.Windows.Forms.Button();
+            this.textBoxRezultat = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // buttonOtvori
+            // 
+            this.buttonOtvori.Location = new System.Drawing.Point(217, 51);
+            this.buttonOtvori.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonOtvori.Name = "buttonOtvori";
+            this.buttonOtvori.Size = new System.Drawing.Size(118, 25);
+            this.buttonOtvori.TabIndex = 0;
+            this.buttonOtvori.Text = "Otvori MessageBox";
+            this.buttonOtvori.UseVisualStyleBackColor = true;
+            this.buttonOtvori.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxRezultat
+            // 
+            this.textBoxRezultat.Location = new System.Drawing.Point(58, 128);
+            this.textBoxRezultat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxRezultat.Name = "textBoxRezultat";
+            this.textBoxRezultat.Size = new System.Drawing.Size(472, 191);
+            this.textBoxRezultat.TabIndex = 1;
+            this.textBoxRezultat.Text = "";
+            this.textBoxRezultat.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // FormDialogResult
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.textBoxRezultat);
+            this.Controls.Add(this.buttonOtvori);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "FormDialogResult";
+            this.Text = "Upotreba DialogResult enumeracije";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOtvori;
+        private System.Windows.Forms.RichTextBox textBoxRezultat;
+    }
+}
+
---
+++ Dialog result kontrola/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DialogResultKontrola
+{
+    public partial class FormDialogResult : Form
+    {
+        public FormDialogResult()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //korisnik nakon pritiska tipke odlučuje "yes ili no"
+            string poruka = "Odaberite Yes / No / Cancel";
+            string naslov = "Naslovna linija Dijaloga za poruke";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNoCancel;
+            MessageBoxIcon ikona = MessageBoxIcon.Information;
+
+            DialogResult rez = MessageBox.Show(poruka, naslov, buttons, ikona);
+
+            //korisnikova odluka se ispisuje u TextBox
+            switch (rez)
+
+            {
+
+                case DialogResult.Yes:
+
+                    textBoxRezultat.Text = "Izabrali ste dugme YES";
+
+                    break;
+
+                case DialogResult.No:
+
+                    textBoxRezultat.Text = "Izabrali ste dugme NO";
+
+                    break;
+
+                case DialogResult.Cancel:
+
+                    textBoxRezultat.Text = "Izabrali ste dugme Cancel";
+
+                    break;
+
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
